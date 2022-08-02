a744ffa6bca89c3295ad2b752476e33b2b75454c
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-08-02 13:05:51 -0300

    Update main.py

--- ota/main.py
+++ ota/main.py
@@ -1,9 +1,7 @@
-print('Teste')
+from machine import TouchPad, Pin
+from time import sleep
 
-# Se voc锚 conseguiu fazer tudo certo, o print acima deve aparecer no relat贸rio do seu equipamento
+tot = TouchPad(Pin(4))
 
-# Procure mudar o que est谩 no script e veja atualizar
-
-# Detalhe: em alguns equipamento entre uma atualiza莽茫o e outra pode levar alguns minutos
-# Na minha ESP32 atualiza uma vez a cada uns 3 minutos sem nenhum erro
-# J谩 na minha LoRa 32 atualiza direto sem intervalos de tempo 
+while True:
+    print(tot.value())
